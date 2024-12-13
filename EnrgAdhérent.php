4e613e126a43f0f31d20be423c2a3ec05a472cc3
Floruss <flo.pecome@gmail.com>
2024-12-13 17:08:42 -0400

    modif basede donnée et php

--- EnrgAdhérent.php
+++ EnrgAdhérent.php
@@ -1,10 +1,10 @@
 <?php
-include("connexionBDD.php");
-$numero_licence = $_POST['numero_licence'];
-$mail = $_POST['mail'];
-$motPasse = $_POST['motPasse'];
+    include("connexionBDD.php");
+    $numero_licence = $_POST['numero_licence'];
+    $mail = $_POST['mail'];
+    $motPasse = $_POST['motPasse'];
 
-$requete = "INSERT INTO Lien (num_licence, mail, mot_passe) 
-VALUES('$numero_licence','$mail','$motPasse')";
-$bdd -> exec( statement: $requete);
+    $requete = "INSERT INTO Lien (num_licence, mail, mot_passe) 
+    VALUES('$numero_licence','$mail','$motPasse')";
+    $bdd -> exec(statement: $requete);
 ?>
