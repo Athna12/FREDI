fcb3f0fa99a92631fb72757b0bb14bd1494f3bef
Nathanaël <nathanaelnagou@gmail.com>
2025-02-10 09:01:42 -0400

    num_licence
    BIGINT en MEDIUMINT

--- NewBDD.sql
+++ NewBDD.sql
@@ -16,7 +16,7 @@ CREATE TABLE `Demandeurs`(
     PRIMARY KEY(`adresse_mail`)
 );
 CREATE TABLE `Lien`(
-    `num_licence` BIGINT UNSIGNED NOT NULL,
+    `num_licence` MEDIUMINT UNSIGNED NOT NULL,
     `adresse_mail` CHAR(255) NOT NULL,
     `mot_passe` VARCHAR(255) NOT NULL
 );
