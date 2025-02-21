cb9a9ae2b5992e7a2c0851b1c6b020fa7fab2dc4
Floruss <flo.pecome@gmail.com>
2025-02-21 14:36:39 -0400

    back to back

--- NewBDD.sql
+++ NewBDD.sql
@@ -1,11 +1,13 @@
-CREATE TABLE `Adhérent`(
+CREATE DATABASE IF NOT EXISTS fredi;
+
+CREATE TABLE IF NOT EXISTS`Adhérent`(
     `numero_licence` INT NOT NULL COMMENT 'NOT NULL',
     `nom` VARCHAR(255) NOT NULL COMMENT 'NOT NULL',
     `prenom` VARCHAR(255) NOT NULL COMMENT 'NOT NULL',
     `numero_ligues` INT NOT NULL,
     PRIMARY KEY(`numero_licence`)
 );
-CREATE TABLE `Demandeurs`(
+CREATE TABLE IF NOT EXISTS `Demandeurs`(
     `adresse_mail` CHAR(255) NOT NULL,
     `nom` CHAR(255) NOT NULL,
     `prenom` CHAR(255) NOT NULL,
@@ -15,12 +17,12 @@ CREATE TABLE `Demandeurs`(
     `num_recu` INT NOT NULL,
     PRIMARY KEY(`adresse_mail`)
 );
-CREATE TABLE `Lien`(
+CREATE TABLE IF NOT EXISTS `Lien`(
     `num_licence` VARCHAR(12) NOT NULL,
     `adresse_mail` CHAR(255) NOT NULL,
     `mot_passe` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `Lignes-frais`(
+CREATE TABLE IF NOT EXISTS`Lignes-frais`(
     `id_adresse_mail` CHAR(255) NOT NULL,
     `date` CHAR(255) NOT NULL,
     `motif` CHAR(255) NOT NULL,
@@ -35,14 +37,14 @@ CREATE TABLE `Lignes-frais`(
     `hebergement_valide` FLOAT(53) NOT NULL,
     PRIMARY KEY(`id_adresse_mail`)
 );
-CREATE TABLE `Ligues`(
+CREATE TABLE IF NOT EXISTS `Ligues`(
     `numero` INT NOT NULL,
     `nom` CHAR(255) NOT NULL,
     `sigle` CHAR(255) NOT NULL,
     `president` CHAR(255) NOT NULL,
     PRIMARY KEY(`numero`)
 );
-CREATE TABLE `Motifs`(
+CREATE TABLE IF NOT EXISTS `Motifs`(
     `libelle` CHAR(255) NOT NULL,
     PRIMARY KEY(`libelle`)
 );
