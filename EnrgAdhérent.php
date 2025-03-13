c6d94e78dba7ceb4ec81ae4c7b53cacaaa56900f
Floruss <flo.pecome@gmail.com>
2025-03-13 16:05:03 -0400

    f

--- EnrgAdhérent.php
+++ EnrgAdhérent.php
@@ -2,7 +2,6 @@
     include("connexionBDD.php");
     $numero_licence = $_POST['numero_licence'];
     $mail = $_POST['adresse_mail'];
-    $motPasse = $_POST['mot_passe'];
     $motPasse = $_POST['motPasse'];
 
     $requete = "INSERT INTO Lien (num_licence, adresse_mail, mot_passe) 
