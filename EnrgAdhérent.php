b8d0d0a85c14916a6a18e854f379028eb4d25275
Floruss <flo.pecome@gmail.com>
2025-03-14 16:14:35 -0400

    Update EnrgAdhérent.php

--- EnrgAdhérent.php
+++ EnrgAdhérent.php
@@ -4,7 +4,7 @@
     $mail = $_POST['adresse_mail'];
     $motPasse = $_POST['motPasse'];
 
-    $requete = "INSERT INTO Lien (num_licence, adresse_mail, mot_passe) 
+    $requete = "INSERT INTO Lien (num_licence, adresse_mail, motPasse) 
     VALUES('$numero_licence','$mail','$motPasse')";
     $bdd -> exec(statement: $requete);
 ?>
