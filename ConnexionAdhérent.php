5a4a27e54504dc220091def5c3792ee1d5e9f1f6
Floruss <flo.pecome@gmail.com>
2025-03-21 15:26:23 -0400

    Connexion Test
    fazf

---
+++ ConnexionAdhérent.php
@@ -0,0 +1,20 @@
+<?php
+// Récupération des données du formulaire
+$mail = $_POST['adresse_mail'];
+$motPasse = $_POST['motPasse'];
+
+// Vérification des informations de connexion
+$sql = "SELECT adresse_mail, motPasse FROM Lien WHERE adresse_mail = :mail";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':adresse_mail' => $mail]);
+$utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (password_verify($motdepasse, 'motdepasse')) {
+    // Connexion réussie
+    header('Location: accueil.php'); // Redirection vers la page d'accueil
+    exit();
+} else {
+    // Échec de la connexion
+    echo "Adresse mail ou mot de passe incorrect.";
+}
+?>
