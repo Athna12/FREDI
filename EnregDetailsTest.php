5ae7962d0c1c44797443e45e7ec2429893239332
Nathanaël <nathanaelnagou@gmail.com>
2025-03-21 16:31:43 -0400

    Creation de test

---
+++ EnregDetailsTest.php
@@ -0,0 +1,25 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class EnregDetailsTest extends TestCase {
+    public function testInsertUtilisateur() {
+        $_POST = [
+            'numero_licence' => '12345',
+            'ligueSportive' => 'Football',
+            'nom' => 'Dupont',
+            'prenom' => 'Jean',
+            'sexe' => 'M',
+            'numTel' => '0606060606',
+            'adresse' => '10 rue des tests',
+            'ville' => 'Paris',
+            'CP' => '75000'
+        ];
+
+        ob_start(); // Démarre la capture de sortie
+        include '../EnregDetails.php'; // Inclut le script PHP à tester
+        $output = ob_get_clean(); // Récupère et vide la sortie du script
+
+        $this->assertEmpty($output, "Le script ne doit pas renvoyer d'erreur.");
+    }
+}
+?>
