70c41d8b0dde5cd4b85e1e5061fb59cd2a419896
Nathanaël <nathanaelnagou@gmail.com>
2025-04-03 12:47:35 -0400

    Suppression de statement

--- PHP/EnrgAdhérent.php
+++ PHP/EnrgAdhérent.php
@@ -6,8 +6,14 @@
 
     $requete = "INSERT INTO Lien (num_licence, adresse_mail, motPasse) 
     VALUES('$numero_licence','$mail','$motPasse')";
-    $bdd -> exec(statement: $requete);
-    // Rediriger vers la page de confirmation après une insertion réussie
+    $bdd->exec($requete);
+    if ($bdd->exec($requete) !== false) {
+        // Rediriger vers la page de confirmation après une insertion réussie
+    } else {
+        // Gérer l'erreur si l'insertion échoue
+        echo "Erreur lors de l'insertion des données.";
+        exit();
+    }
     header("Location: confirmation création de compte.html");
     exit(); // Assurez-vous de terminer le script après la redirection
     ?>
