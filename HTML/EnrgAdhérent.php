809118a2f320fd82d4b97b3fab9f7478ab7e3465
Nathanaël <nathanaelnagou@gmail.com>
2025-04-11 10:37:27 -0400

    plus de vérification

--- HTML/EnrgAdhérent.php
+++ HTML/EnrgAdhérent.php
@@ -4,16 +4,14 @@
     $mail = $_POST['adresse_mail'];
     $motPasse = $_POST['motPasse'];
 
-    $requete = "INSERT INTO Lien (num_licence, adresse_mail, motPasse) 
+    $requete = "INSERT INTO Lien (num_licence, adresse_mail, mot_passe) 
     VALUES('$numero_licence','$mail','$motPasse')";
-    $bdd->exec($requete);
+    
     if ($bdd->exec($requete) !== false) {
-        // Rediriger vers la page de confirmation après une insertion réussie
+        header("Location: confirmation création de compte.html");
+        exit();
     } else {
-        // Gérer l'erreur si l'insertion échoue
         echo "Erreur lors de l'insertion des données.";
         exit();
     }
-    header("Location: confirmation création de compte.html");
-    exit(); // Assurez-vous de terminer le script après la redirection
-    ?>
+?>
