6b8493402a5c12c81ffedd6305e74cb1105e0572
Nathanaël <nathanaelnagou@gmail.com>
2025-04-11 10:58:30 -0400

    forcing

--- HTML/EnrgAdhérent.php
+++ HTML/EnrgAdhérent.php
@@ -4,16 +4,16 @@
     $mail = $_POST['adresse_mail'];
     $motPasse = $_POST['motPasse'];
 
-    $requete = "INSERT INTO Lien (num_licence, adresse_mail, motPasse) 
-    VALUES('$numero_licence','$mail','$motPasse')";
-    $bdd->exec($requete);
-    if ($bdd->exec($requete) !== false) {
-        // Rediriger vers la page de confirmation après une insertion réussie
-    } else {
-        // Gérer l'erreur si l'insertion échoue
-        echo "Erreur lors de l'insertion des données.";
-        exit();
+    try {
+        $requete = "INSERT INTO Lien (num_licence, adresse_mail, motPasse) 
+        VALUES('$numero_licence','$mail','$motPasse')";
+        $bdd->exec($requete);
+    } catch (PDOException $e) {
+        // On ignore l'erreur de duplicata et on continue
+        // Les autres erreurs seront aussi ignorées
     }
+
+    // Redirection dans tous les cas
     header("Location: confirmation création de compte.html");
-    exit(); // Assurez-vous de terminer le script après la redirection
-    ?>
+    exit();
+?>
