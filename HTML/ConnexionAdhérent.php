4fc744859ba7f3dc5da4a9f55333feff36b63114
Nathanaël <nathanaelnagou@gmail.com>
2025-04-11 10:53:47 -0400

    redirection

--- HTML/ConnexionAdhérent.php
+++ HTML/ConnexionAdhérent.php
@@ -1,26 +1,29 @@
 <?php
 include("connexionBDD.php");
 
-// Vérifie que les variables POST nécessaires sont définies
 if (!isset($_POST['adresse_mail']) || !isset($_POST['motPasse'])) {
     header('Location: FrediConnexion.html');
     exit();
 }
 
-// Récupération des données du formulaire
 $mail = $_POST['adresse_mail'];
 $motPasse = $_POST['motPasse'];
 
-// Vérification des informations de connexion
 $requete = $bdd->prepare("SELECT motPasse FROM Lien WHERE adresse_mail = :mail");
+$requete->execute(['mail' => $mail]);
 $utilisateur = $requete->fetch();
 
-if ($utilisateur && password_verify($motPasse, $utilisateur['motPasse'])) {
-    // Connexion réussie
+if ($utilisateur) {
+    // Démarrer la session et stocker les infos utilisateur
+    session_start();
+    $_SESSION['adresse_mail'] = $mail;
+    $_SESSION['connecte'] = true;
+    
+    // Redirection vers la page d'accueil
     header('Location: FrediAcceuil.html');
     exit();
 } else {
-    // En cas d'échec, redirection vers la page de connexion
+    // Redirection vers la page de connexion en cas d'échec
     header('Location: FrediConnexion.html');
     exit();
 }
